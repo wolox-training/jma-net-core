ea9124b33fded03798335a96c369e23cc1258d1b
Juan Manuel Alonso <juanmanuel.alonso@wolox.com.ar>
2018-09-10 10:52:16 -0300

    gitignore. Movie model. DataBaseContext repository. Services at Startup.

--- src/Startup.cs
+++ src/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using testing_net.Repositories.Database;
 
 namespace testing_net
 {
@@ -33,6 +34,8 @@ namespace testing_net
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<DataBaseContext>(options =>  options.UseNpgsql(Configuration["ConnectionString"]));
+            services.AddScoped<DataBaseContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
