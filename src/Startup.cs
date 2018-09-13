ff2f91ff78890ef0ab79786ec8ed8427707f1a01
Juan Manuel Alonso <juanmanuel.alonso@wolox.com.ar>
2018-09-13 11:21:50 -0300

    Spacing and CultureInfo.CurrentUICulture.

--- src/Startup.cs
+++ src/Startup.cs
@@ -42,16 +42,10 @@ namespace testing_net
 
             services.AddJsonLocalization(options => options.ResourcesPath = "Resources");
             services.AddMvc().AddViewLocalization();
-<<<<<<< e6343d9b9d32ed2ede0b13a01d791905cc02e76b
-            CultureInfo.CurrentUICulture = new CultureInfo(Configuration["DefaultLang"]);   
-        }
-=======
-            CultureInfo.CurrentCulture = new CultureInfo(Configuration["DefaultLang"]);
+            CultureInfo.CurrentUICulture = new CultureInfo(Configuration["DefaultLang"]);
 
-            services.AddScoped<IUnitOfWork, UnitOfWork>();  
-            
-            }
->>>>>>> Repository and UnitOfWork patterns. Repository structure. MovieRepository.
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
