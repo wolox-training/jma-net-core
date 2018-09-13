aa8275a75921008defc9e39a0a44593b78cf9b4b
Juan Manuel Alonso <juanmanuel.alonso@wolox.com.ar>
2018-09-13 13:44:40 -0300

    Create movie endpoint. Connecting UoW  with Controller, View and MovieViewModel.

--- src/Models/Views/MovieViewModel.cs
+++ src/Models/Views/MovieViewModel.cs
@@ -1,6 +1,9 @@
 using System;
+<<<<<<< 6a10f5dd259dd895bf5d1d86e08aa02da504297f
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+=======
+>>>>>>> Create movie endpoint. Connecting UoW  with Controller, View and MovieViewModel.
 
 namespace testing_net.Models.Views
 {
@@ -11,6 +14,7 @@ namespace testing_net.Models.Views
         [StringLength(60, MinimumLength = 3)]
         [Required]
         public string Title { get; set; }
+<<<<<<< 6a10f5dd259dd895bf5d1d86e08aa02da504297f
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
@@ -24,6 +28,10 @@ namespace testing_net.Models.Views
         [Range(1, 100)]
         [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
+=======
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+>>>>>>> Create movie endpoint. Connecting UoW  with Controller, View and MovieViewModel.
         public decimal Price { get; set; }
         
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
