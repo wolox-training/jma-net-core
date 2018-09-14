3038068a15d159cd96add5209b7d13d3f9faf152
Juan Manuel Alonso <juanmanuel.alonso@wolox.com.ar>
2018-09-14 14:44:31 -0300

    DataAnnotations for Movie.

--- src/Models/Views/MovieViewModel.cs
+++ src/Models/Views/MovieViewModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace testing_net.Models.Views
 {
@@ -6,8 +8,13 @@ namespace testing_net.Models.Views
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
