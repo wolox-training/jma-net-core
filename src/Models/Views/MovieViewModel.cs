1030f047a1d732bbfbf0c5a717fe7b849abdddd5
Juan Manuel Alonso <juanmanuel.alonso@wolox.com.ar>
2018-09-20 10:14:57 -0300

    Conflict syntax was pushed accidentally from previous rebase.

--- src/Models/Views/MovieViewModel.cs
+++ src/Models/Views/MovieViewModel.cs
@@ -1,9 +1,6 @@
 using System;
-<<<<<<< 6a10f5dd259dd895bf5d1d86e08aa02da504297f
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-=======
->>>>>>> Create movie endpoint. Connecting UoW  with Controller, View and MovieViewModel.
 
 namespace testing_net.Models.Views
 {
@@ -11,7 +8,6 @@ namespace testing_net.Models.Views
     {
         public int ID { get; set; }
         public string Title { get; set; }
-<<<<<<< 6a10f5dd259dd895bf5d1d86e08aa02da504297f
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
@@ -19,10 +15,6 @@ namespace testing_net.Models.Views
         public string Genre { get; set; }
         
         [Column(TypeName = "decimal(18, 2)")]
-=======
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
->>>>>>> Create movie endpoint. Connecting UoW  with Controller, View and MovieViewModel.
         public decimal Price { get; set; }
     }
 }
